e332c379b57867a6c741071dfdbcf9e80b2d0c15
Khaf-dev <rifyatkaffa@gmail.com>
2026-01-02 17:21:25 +0700

    new file kode: ops_config_dto.go

---
+++ backend/internal/app/dto/ops_config_dto.go
@@ -0,0 +1,8 @@
+package dto
+
+type OpsConfigDTO struct {
+	LeaderName string `json:"leader_name,omitempty"`
+	Status     string `json:"status,omitempty"`
+	Location   string `json:"location,omitempty"`
+	
+}
